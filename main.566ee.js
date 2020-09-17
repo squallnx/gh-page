1ec6a8b629d5e608ebbf1f8313fb01f074a1c944
squallnx <ngxuanhieu2004@gmail.com>
2020-09-17 21:57:26 +0700

    update

--- main.566ee.js
+++ main.566ee.js
@@ -141,7 +141,7 @@ window.boot = function () {
     // jsList
     var jsList = settings.jsList;
 
-    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.ecf05.js';
+    var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.b9f33.js';
     if (jsList) {
         jsList = jsList.map(function (x) {
             return 'src/' + x;
